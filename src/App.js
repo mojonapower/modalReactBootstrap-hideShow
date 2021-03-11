ffea5557366964781ff866a6ce2b732d1cbf3925
mojonapower <maca95@gmail.com>
2021-03-11 13:32:27 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Ventana } from "./modal";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Ventana />
+    </div>
+  );
+}
---
+++ src/modal.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+export const Ventana = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        ESTE ES MI ONCLICK! ESTOY DENTRO DEL COMPONENTE pero! si me llamas no se
+        verá de inmediato el modal, solo cuando me hagas click
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
